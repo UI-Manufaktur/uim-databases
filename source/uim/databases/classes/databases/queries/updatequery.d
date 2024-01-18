7eb9cf48c81c011f7f494b780c5cb0b55fb29773
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-18 06:45:40 +0100

    Adding files from cake

---
+++ source/uim/databases/classes/databases/queries/updatequery.d
@@ -0,0 +1,97 @@
+module uim.cake.databases.Query;
+
+import uim.cake;
+
+@safe:
+
+/*
+ */
+
+// This class is used to generate UPDATE queries for the relational database.
+class UpdateQuery : Query {
+    // Type of this query.
+    protected string _type = self.TYPE_UPDATE;
+
+    /**
+     * List of SQL parts that will be used to build this query.
+     */
+    protected Json[string] _parts = [
+        "comment": null,
+        "with": [],
+        "update": [],
+        "modifier": [],
+        "join": [],
+        "set": [],
+        "where": null,
+        "order": null,
+        "limit": null,
+        "epilog": null,
+    ];
+
+    /**
+     * Create an update query.
+     *
+     * Can be combined with set() and where() methods to create update queries.
+     * Params:
+     * \UIM\Database\IExpression|string atable The table you want to update.
+     */
+    void update(IExpression|string atable) {
+       _dirty();
+       _parts["update"][0] = aTable;
+Y>    }
+    
+    /**
+     * Set one or many fields to update.
+     *
+     * ### Examples
+     *
+     * Passing a string:
+     *
+     * ```
+     * aQuery.update("articles").set("title", "The Title");
+     * ```
+     *
+     * Passing an array:
+     *
+     * ```
+     * aQuery.update("articles").set(["title": 'The Title"], ["title": `string"]);
+     * ```
+     *
+     * Passing a callback:
+     *
+     * ```
+     * aQuery.update("articles").set(function ($exp) {
+     *  return $exp.eq("title", "The title", "string");
+     * });
+     * ```
+     * Params:
+     * \UIM\Database\Expression\QueryExpression|\Closure|string[] aKey The column name or array of keys
+     *   + values to set. This can also be a QueryExpression containing a SQL fragment.
+     *   It can also be a Closure, that is required to return an expression object.
+     * @param Json aValue The value to update aKey to. Can be null if aKey is an
+     *   array or QueryExpression. When aKey is an array, this parameter will be
+     *   used as $types instead.
+     * @param STRINGAA|string atypes The column types to treat data as.
+     */
+    void set(QueryExpression|Closure|string[] aKey, Json aValue = null, string[] atypes = []) {
+        if (isEmpty(_parts["set"])) {
+           _parts["set"] = this.newExpr().setConjunction(",");
+        }
+        if (cast(Closure)aKey) {
+            $exp = this.newExpr().setConjunction(",");
+           _parts["set"].add(aKey($exp));
+
+            return ;
+        }
+        if (isArray(aKey) || cast(IExpression)aKey ) {
+            $types = (array)aValue;
+           _parts["set"].add(aKey, $types);
+
+            return ;
+        }
+        if (!isString($types)) {
+            $types = null;
+        }
+       _parts["set"].eq(aKey, aValue, $types);
+    }
+}
---
+++ source/uim/databases/classes/databases/retries/reconnectstrategy.d
@@ -0,0 +1,94 @@
+module uim.cake.databases.Retry;
+
+import uim.cake;
+
+@safe:
+
+/*
+
+/**
+ * Makes sure the connection to the database is alive before authorizing
+ * the retry of an action.
+ *
+ * @internal
+ */
+class ReconnectStrategy : IRetryStrategy {
+    /**
+     * The list of error strings to match when looking for a disconnection error.
+     *
+     * This is a static variable to enable opcache to inline the values.
+     */
+    protected static string[] $causes = [
+        "gone away",
+        "Lost connection",
+        "Transaction() on null",
+        "closed the connection unexpectedly",
+        "closed unexpectedly",
+        "deadlock avoided",
+        "decryption failed or bad record mac",
+        "is dead or not enabled",
+        "no connection to the server",
+        "query_wait_timeout",
+        "reset by peer",
+        "terminate due to client_idle_limit",
+        "while sending",
+        "writing data to the connection",
+    ];
+
+    // The connection to check for validity
+    protected Connection _connection;
+
+    /**
+     * Creates the ReconnectStrategy object by storing a reference to the
+     * passed connection. This reference will be used to automatically
+     * reconnect to the server in case of failure.
+     * Params:
+     * \UIM\Database\Connection aConnection The connection to check
+     */
+    this(Connection aConnection) {
+       _connection = aConnection;
+    }
+    
+    /**
+     * Checks whether the exception was caused by a lost connection,
+     * and returns true if it was able to successfully reconnect.
+     */
+    bool shouldRetry(Exception $exception, int $retryCount) {
+        auto $message = $exception.getMessage();
+
+        foreach ($cause; $causes) {
+            if ($message.has($cause)) {
+                return this.reconnect();
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Tries to re-establish the connection to the server, if it is safe to do so
+     */
+    protected bool reconnect() {
+        if (this.connection.inTransaction()) {
+            // It is not safe to blindly reconnect in the middle of a transaction
+            return false;
+        }
+        try {
+            // Make sure we free any resources associated with the old connection
+            this.connection.getDriver().disconnect();
+        } catch (Exception) {
+        }
+        try {
+            this.connection.getDriver().connect();
+            this.connection.getDriver().log(
+                'connection={connection} [RECONNECT]",
+                ["connection": this.connection.configName()]
+            );
+
+            return true;
+        } catch (Exception) {
+            // If there was an error connecting again, don`t report it back,
+            // let the retry handler do it.
+            return false;
+        }
+    }
+}
