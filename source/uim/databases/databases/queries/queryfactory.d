abdffb3fe58e5db92699eea91eccb8ba568ba265
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-25 14:10:42 +0100

    Updates

---
+++ source/uim/databases/databases/queries/queryfactory.d
@@ -0,0 +1,103 @@
+module uim.cake.databases.Query;
+
+import uim.cake;
+
+@safe:
+
+//Factory class for generating instances of Select, Insert, Update, Delete queries.
+class QueryFactory {
+    
+    this(
+        protected Connection aConnection,
+    ) {
+    }
+    
+    /**
+     * Create a new SelectQuery instance.
+     * Params:
+     * \UIM\Database\IExpression|\Closure|string[]|float|int fields Fields/columns list for the query.
+     * @param string[] atable List of tables to query.
+     * typesForCasting Associative array containing the types to be used for casting.
+     */
+    SelectQuery select(
+        IExpression|Closure|string[]|float|int fields = [],
+        string[] atable = [],
+        STRINGAA typesForCasting = []
+    ) {
+        aQuery = new SelectQuery(this.connection);
+
+        aQuery
+            .select($fields)
+            .from(aTable)
+            .setDefaultTypes(typesForCasting);
+
+        return aQuery;
+    }
+    
+    /**
+     * Create a new InsertQuery instance.
+     * Params:
+     * string aTable The table to insert rows into.
+     * @param array  someValues Associative array of column: value to be inserted.
+     * typesForCasting Associative array containing the types to be used for casting.
+     */
+    InsertQuery insert(string atable = null, array  someValues = [], STRINGAA typesForCasting = []) {
+        aQuery = new InsertQuery(this.connection);
+
+        if (aTable) {
+            aQuery.into(aTable);
+        }
+        if (someValues) {
+            someColumns = array_keys(someValues);
+            aQuery
+                .insert(someColumns, typesForCasting)
+                .values(someValues);
+        }
+        return aQuery;
+    }
+    
+    /**
+     * Create a new UpdateQuery instance.
+     * Params:
+     * \UIM\Database\IExpression|string aTable The table to update rows of.
+     * @param array  someValues Values to be updated.
+     * @param array conditions Conditions to be set for the update statement.
+     * typesForCasting Associative array containing the types to be used for casting.
+     */
+    UpdateQuery update(
+        IExpression|string aTable = null,
+        array  someValues = [],
+        array conditions = [],
+        typesForCasting = []
+    ) {
+        aQuery = new UpdateQuery(this.connection);
+
+        if (aTable) {
+            aQuery.update(aTable);
+        }
+        if (someValues) {
+            aQuery.set(someValues, typesForCasting);
+        }
+        if ($conditions) {
+            aQuery.where($conditions, typesForCasting);
+        }
+        return aQuery;
+    }
+    
+    /**
+     * Create a new DeleteQuery instance.
+     * Params:
+     * string aTable The table to delete rows from.
+     * @param array conditions Conditions to be set for the delete statement.
+     * typesForCasting Associative array containing the types to be used for casting.
+     */
+    DeleteQuery delete(string atable = null, array conditions = [], STRINGAA typesForCasting = []) {
+        aQuery = (new DeleteQuery(this.connection))
+            .delete(aTable);
+
+        if ($conditions) {
+            aQuery.where($conditions, typesForCasting);
+        }
+        return aQuery;
+    }
+}
---
+++ source/uim/databases/databases/schemas/collection.d
@@ -0,0 +1,131 @@
+module uim.cake.databases.schemas;
+
+import uim.cake;
+
+@safe:
+
+/*
+
+/**
+ * Represents a database schema collection
+ *
+ * Used to access information about the tables,
+ * and other data in a database.
+ */
+class Collection : ICollection {
+    // Connection object
+    protected Connection _connection;
+
+    // Schema dialect instance.
+    protected SchemaDialect _dialect;
+
+    /**
+     * Constructor.
+     * Params:
+     * \UIM\Database\Connection aConnection The connection instance.
+     */
+    this(Connection newConnection) {
+       _connection = newConnection;
+       _dialect = newConnection.getDriver().schemaDialect();
+    }
+    
+    /**
+     * Get the list of tables, excluding any views, available in the current connection.
+     * The list of tables in the connected database/schema.
+     */
+    string[] listTablesWithoutViews() {
+        [$sql, params] = _dialect.listTablesWithoutViewsSql(_connection.getDriver().config());
+        auto result;
+        statement = _connection.execute($sql, params);
+        while ($row = statement.fetch()) {
+            result ~= row[0];
+        }
+        return result;
+    }
+    
+    /**
+     * Get the list of tables and views available in the current connection.
+     */
+    string[] listTables() {
+        [$sql, params] = _dialect.listTablesSql(_connection.getDriver().config());
+        auto result;
+        statement = _connection.execute($sql, params);
+        while ($row = statement.fetch()) {
+            result ~= row[0];
+        }
+        return result;
+    }
+    
+    /**
+     * Get the column metadata for a table.
+     *
+     * The name can include a database schema name in the form `schema.table'.
+     *
+     * Caching will be applied if `cacheMetadata` key is present in the Connection
+     * configuration options. Defaults to _cake_model_when true.
+     *
+     * ### Options
+     *
+     * - `forceRefresh` - Set to true to force rebuilding the cached metadata.
+     *  Defaults to false.
+     * Params:
+     * string aName The name of the table to describe.
+     * @param Json[string] options The options to use, see above.
+     */
+    TableISchema describe(string aName, Json[string] options = null) {
+        configData = _connection.config();
+        if ($name.has(".")) {
+            [configData("schema"], name] = split(".", name);
+        }
+        aTable = _connection.getDriver().newTableSchema($name);
+
+       _reflect("Column", name, configData, aTable);
+        if (count(aTable.columns()) == 0) {
+            throw new DatabaseException("Cannot describe %s. It has 0 columns.".format($name));
+        }
+       _reflect("Index", name, configData, aTable);
+       _reflect("ForeignKey", name, configData, aTable);
+       _reflect("Options", name, configData, aTable);
+
+        return aTable;
+    }
+    
+    /**
+     * Helper method for running each step of the reflection process.
+<<<<<<< HEAD
+     * Params:
+     * string astage The stage name.
+     * @param string aName The table name.
+=======
+     *
+     * @param string astage The stage name.
+>>>>>>> 6c77f0755345c40125ff261f87e4ab75fc24b38d
+     * configData - The config data.
+     * @param \UIM\Database\Schema\TableISchema tableSchema The table schema instance.
+     * @throws \UIM\Database\Exception\DatabaseException on query failure.
+     * @uses \UIM\Database\Schema\SchemaDialect.describeColumnSql
+     * @uses \UIM\Database\Schema\SchemaDialect.describeIndexSql
+     * @uses \UIM\Database\Schema\SchemaDialect.describeForeignKeySql
+     * @uses \UIM\Database\Schema\SchemaDialect.describeOptionsSql
+     * @uses \UIM\Database\Schema\SchemaDialect.convertColumnDescription
+     * @uses \UIM\Database\Schema\SchemaDialect.convertIndexDescription
+     * @uses \UIM\Database\Schema\SchemaDialect.convertForeignKeyDescription
+     * @uses \UIM\Database\Schema\SchemaDialect.convertOptionsDescription
+     */
+    protected void _reflect(string astage, string tableName, IConfigData[string] configData, TableISchema tableSchema) {
+        string describeMethod = "describe{$stage}Sql";
+        string convertMethod = "convert{$stage}Description";
+
+        [$sql, params] = _dialect.{describeMethod}(tableName, configData);
+        if (isEmpty($sql)) {
+            return;
+        }
+        try {
+            statement = _connection.execute($sql, params);
+        } catch (PDOException  anException) {
+            throw new DatabaseException(anException.getMessage(), 500,  anException);
+        }
+        statement.fetchAll("assoc")
+            .each!(row => _dialect.{convertMethod}(tableSchema, row));
+    }
+}
