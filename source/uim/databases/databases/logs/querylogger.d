8724067aa837ba3dc757b7f31f5f968225fa388c
UIMSolutions <ui.manufaktur@gmail.com>
2024-01-25 14:10:29 +0100

    Updates

---
+++ source/uim/databases/databases/logs/querylogger.d
@@ -0,0 +1,34 @@
+module uim.cake.databases.logs.queryloggers;
+
+import uim.cake;
+
+@safe:
+
+/**
+ * This class is a bridge used to write LoggedQuery objects into a real log.
+ * by default this class use the built-in UIM Log class to accomplish this
+ */
+class QueryLogger : BaseLog {
+
+  this(IConfigData[string] configData = null) {
+    _defaultConfig["scopes"] = ["queriesLog", "cake.database.queries"];
+    _defaultConfig["connection"] = "";
+
+    super(configData);
+  }
+
+  void log($level, string | Stringable$message, array mycontext = []) {
+    context += [
+      "scope": this.scopes() ? : ["queriesLog", "cake.database.queries"],
+      "connection": _configData.isSet("connection"),
+      "query": null,
+    ];
+
+    if (cast(LoggedQuery)$context["query"]) {
+      context = context["query"].getContext() + context;
+      string message = "connection={connection} role={role} duration={took} rows={numRows} " ~ message;
+    }
+
+    Log.write("debug", to!string($message), context);
+  }
+}
---
+++ source/uim/databases/databases/queries/deletequery.d
@@ -0,0 +1,40 @@
+module uim.cake.databases.Query;
+
+import uim.cake;
+
+@safe:
+
+// This class is used to generate DELETE queries for the relational database.
+class DeleteQuery : Query {
+    // Type of this query.
+    protected string _type = self.TYPE_DELETE;
+
+    // List of SQL parts that will be used to build this query.
+    protected Json[string] _parts = [
+        "comment": null,
+        "with": [],
+        "delete": true,
+        "modifier": [],
+        "from": [],
+        "join": [],
+        "where": null,
+        "order": null,
+        "limit": null,
+        "epilog": null,
+    ];
+
+    /**
+     * Create a delete query.
+     *
+     * Can be combined with from(), where() and other methods to
+     * create delete queries with specific conditions.
+     * Params:
+     * string aTable The table to use when deleting.
+     */
+    void delete(string atable = null) {
+       _dirty();
+        if (!aTable.isNull) {
+            this.from(aTable);
+        }
+    }
+}
